9ebadf74f13528cf8fd334aed4d1591983ea9e88
Eric Yan <long@ericyan.me>
2017-10-20 18:25:25 +0800

    Manage defined values via environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -e
 
+prefix='DEF_'
+file='/etc/kamailio/kamailio-local.cfg'
+cat > $file
+printenv | grep "^$prefix" | while read -r env; do
+  k=${env%=*}
+  v=${env#*=}
+
+  case ${v,,} in
+    true|yes|on)  v=1 ;;
+    false|no|off) v=0 ;;
+  esac
+
+  def="#!define ${k#$prefix}"
+  case $v in
+    '')      ;;
+    *[0-9]*) def="$def $v" ;;
+    *)       def="$def \"$v\"" ;;
+  esac
+
+  echo $def >> $file
+done
+
 if [ "$1" = 'kamailio' ]; then
   exec chroot --userspec=kamailio / /usr/sbin/kamailio -DD -E -m $SHM_MEMORY -M $PKG_MEMORY -u kamailio -g kamailio
 fi
